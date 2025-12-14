97cea6b2934bdb39cc98f73eaeeb4cedc01a10a0
Tejas Rana <tejasrana95@gmail.com>
2025-12-14 17:28:36 +0530

    fix scss issue

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
+import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { NotificationProvider } from "@/contexts/NotificationContext";
 import { CurrencyProvider } from "@/contexts/CurrencyContext";
