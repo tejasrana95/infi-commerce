03461ae1552ce19b0581f8af89c97ab09fce609a
Tejas Rana <tejasrana95@gmail.com>
2026-01-13 14:23:03 +0530

    fix build error

--- backend/src/middleware/checkDemoMode.ts
+++ backend/src/middleware/checkDemoMode.ts
@@ -2,7 +2,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from './validation';
 
-export const checkDemoMode = (req: Request, res: Response, next: NextFunction) => {
+export const checkDemoMode = (_req: Request, _res: Response, next: NextFunction) => {
     if (process.env.DEMO === 'true') {
         return next(new AppError('This action is restricted in demo mode', 403));
     }
